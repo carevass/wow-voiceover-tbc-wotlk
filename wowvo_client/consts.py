fddbf49d6fd6c18d396df280875cc2e19341f2ec
carevass <12954428+carevass@users.noreply.github.com>
2026-06-03 09:27:54 -0400

    add new word replacements, voice_name mappings, and npc effect mappings based on playback in-game.

--- wowvo_client/consts.py
+++ wowvo_client/consts.py
@@ -76,6 +76,7 @@ REPLACE_DICT = {
                 '<race>': 'traveler', '<Race>': 'Traveler',
                 '<class>': 'adventurer', '<Class>': 'Adventurer',
                  '—':',', '--':',', " - ":", ",
+                 
                  # Factions / Regions
                  "Draenei": "Dray-nai",
                 "Lordaeron": "Lor-deron",
@@ -97,6 +98,8 @@ REPLACE_DICT = {
                 "ursoc":"ursok",
                 "dun argol":"duunar-goll",
                 "earthen":"ehrrthin",
+                "gryphon":"griffon",
+                "tyrannus":"tyerannus",
 
                 # Bosses / NPCs
                 "Malygos": "Maali-goss",
@@ -110,7 +113,7 @@ REPLACE_DICT = {
                 "C'Thun": "Kuh-thoon",
                 "Yogg-Saron": "Yog-suh-ron",
                 "Gjalerbron": "Yal-er-bron",
-                "Heb'Drakkar": "Heb-drah-kar",
+                "Heb'Drakkar": "Hebdrah-kar",
                 "Rageclaw": "Rage-claw",
                 "Ragemane": "Rage-mane",
                 "Verna":"Vur-nah",
@@ -125,14 +128,16 @@ REPLACE_DICT = {
                 "undeath":"on-deth",
                 "Lok'tar ogar":"Loktaro garr",
                 'mrgl-mrgl':"mergle-mergle",
-
+                "sseratus":'seratus',
+                "sargeras":"sargheras",
+                "mac'aree":"macka-ree",
+                "ata'mal":"atah-maal",
                 # Places
-                #"Azeroth":"Ah-ze-roth",
                 "Icecrown": "Ice crown",
                 "Dragonshrine": "Dragon-shrine",
                 "Auchindoun": "Aw-kin-doon",
                 "Hyjal": "High-jahl",
-                "Mathystra": "Math-is-trah",
+                "Mathystra": "Mathis-trah",
                 "Ulduar": "Ool-dwar",
                 "Utgarde": "Oot-guard",
                 "Zul'Aman": "Zool-ahmaan",
@@ -144,17 +149,20 @@ REPLACE_DICT = {
                 "Elwynn":"Elwin",
                 "Arcatraz":"Arc-a-traz",
                 "Stonetalon":"stone-talon",
-                "Kalimdor":"Kalim-dor",
+                "Kalimdor":"Kaalim-dor",
 
                 # Titans / Lore
                 "Tyr": "Teer",
-                "Freya": "Fray-ah",
-                "Thorim": "Thor-rim",
+                "Freya": "Frayah",
                 "Hodir": "Ho-deer",
-                #"Loken": "Low-ken",
+                "mimir":"mimeer",
                 "Ymiron": "Yee-miron",
                 "Elune":"Ehloon",
-
+                "ymirheim":"yeemir-heim",
+                "midrealm":"mid-relm",
+                "mechagnomes":"mecca-nomes",
+                "mechazod":"mecca-zod",
+                "dragonblight":"dragon-blight",
                 # Misc
                 "Felwood": "Fell-wood",
                 "Ashenvale": "Ashen-veil",
@@ -162,6 +170,8 @@ REPLACE_DICT = {
                 "Sin'dorei":"Sindoh-rye",
                 "Gorefiend":"Gorfeend",
                 "Indu'le": "Indu-lay",
+                "goretalon": "gorr-talon",
+                "shan'do":"shan'doe",
 
 
 }
@@ -175,20 +185,22 @@ REUSE_AUDIO_MAP = {
     "banshee": "forsaken_female",
     "mountain_giant": "ancient",
     "orc_hero": "felorc_male",
+    "girl":"goblin_female",
+    "ethereal_stalker":"draenei_male",
+    "naga_male":"broken_male",
 }
 VOICE_MODEL_MAP = {
     # sholazar
     "wolvar_male": "rexxar",
     "gorloc_male": "furbolg_male",
+    "cairne":"big_creature",
 
     #dragon
     "dragon_female":"tauren_female",
     "dragon_male":"demon_male",
 
-
     # big creature shared model
     "tooga":"big_creature",
-
     "giant_male": "big_creature",
     "vrykul_male": "orc_male",
     "ogre_male": "felorc_male",
@@ -197,22 +209,32 @@ VOICE_MODEL_MAP = {
     "mountain_giant_dk": "felorc_male", #for rune giants, e.g., Gavrock
     #other
     "giant_female":"forsaken_female",
+    "giant_female_dk":"forsaken_female",
+    "bone_witch":"forsaken_female",
     "titan_male":"varian",
-    "medivh":"khadgar",
+    "matador":"ethereal_stalker",
     "ogrila_ogre":'khadgar',
-    "earthen":"dwarf_male",
+    "earthen":"tuskarr_male",
     "naaru":"tauren_female",
     "murloc":"demon_female",
+    "naga_male":"demon_female",
+    "naga_male_dk":"demon_female",
     "fire_elemental":"demon_male",
     "water_elemental":"demon_male",
     "earth_elemental":"demon_male",
     "wind_elemental":"demon_male",
     "wolf":"rexxar",
     "bear":"rexxar",
+    "mammoth":"big_creature",
     "cat":"tauren_female",
     "rhino":"tauren_male",
-    "serpent":"naga_female",
+    "serpent":"demon_female",
     "banshee":"forsaken_female",
+    "geist":"arakkoa_male",
+    "demon_boy":"boy",
+    "sanlayn":"bloodelf_male",
+    "sporeggar":"furbolg_male",
+    "akama":"broken_male",
 }
 
 
@@ -236,6 +258,11 @@ NPC_EFFECTS = {
     13716:"ghost",
     14470:"demon",
     14902:"giant",
+    10929:"demon", #Haleh
+    10976:"demon", #Jeziba
+    15362:"ghost", #malfurion stormrage
+    14347:"demon", #highlord demetrian
+    180642:"underwater", #inconspicuous crate (gnome_male)
     16015:"demon",
     14354:"demon",
     16201: "ghost",
@@ -246,20 +273,28 @@ NPC_EFFECTS = {
     17712: "ghost",
     17674:"ghost",
     17877:"ancient",
-    187565:"ancient",
-    18369:"small",
-    18445:"small",
+    187565:"ghost", #elder atkanok
+    18369:"small", #corki
+    18445:"small", #corki
     18687:"ghost",
-    20812:"small",
-    21700:"demon",
+    20812:"small", #corki
     21797:"demon",
     21318:"ghost",
-    21330:"demon",
+    185126:"ghost", #crimson sigil crystal prison
     22103:"demon",
     23778:"undead",
     24137:"undead",
     25425:"ghost",
-    26117:"demon",
+    22113:"demon", #mordenai
+    23433:"demon", #barthamus
+    23141:"demon", #yarzill the merc
+    26117:"demon", #raelorasz
+    27658:"demon", #belgaristrasz
+    28012:"ghost", #image of belgaristrasz stage 1
+    27657:"demon", #verdisa
+    27659:"demon", #eternos
+    30227:"demon", #penumbrius
+    32548:"demon", #corastrasza
     26206:"demon",
     24910:"ghost",
     26501:"ghost",
@@ -269,17 +304,28 @@ NPC_EFFECTS = {
     31135:"ghost",
     19456:"ghost",
     19644:"ghost",
-    19488:"undead",
-    19489:"undead",
-    20463:"undead",
-    20464:"undead",
-    20482:"comms",
+    27229:"ghost", #forgotten footman
+    27224:"ghost", #forgotten knight
+    27225:"ghost", #forgotten rifleman
+    27226:"ghost", #forgotten peasant
+    23730:"underwater", #harold lagras
+    29455:"underwater", #gerk
+    19488:"undead", #custodian dieworth
+    19489:"undead", #lieutenant-sorcerer morran
+    20463:"undead", #apprentice andrethan
+    20464:"undead", #thadell
+    20482:"comms", #image of commander ameer
+    20154:"undead", #shrouded figure
+    20110:"demon", #tyri
     20518:"comms",
     20084:"comms",
-    28377:"undead",
-    28943:"undead",
-    28907:"undead",
-    28911:"undead",
+    19698:"ghost", #greatfather aldrimus
+    20130:"demon", #andormu (boy)
+    18723:"demon", #erozion (old hillsbrad)
+    19935:"demon", #soridormi
+    20201:"demon", #sa'at (black morass)
+    27915:"demon", #chromie (cot by strath)
+    38589:"demon", #valithria dreamwalker
     19937:"ghost",
     29259:"ghost",
     24027:"undead",
@@ -287,6 +333,10 @@ NPC_EFFECTS = {
     24261:"wolf", #ulfang
     27275:"bear", #kodian
     27274:"bear", #orsonn
+    28030:"serpent", #quetz'lun's spirit
+    28401:"cat", #har'koa
+    28561:"bear", #spirit of rhunok
+    25862:"mammoth", #khu'nok
     27350:"demon",
     26443:"demon",
     27950:"demon",
@@ -294,6 +344,14 @@ NPC_EFFECTS = {
     27575:"demon",
     27990:"demon",
     27785:"demon",
+    27506:"demon", #ceristrasz
+    16816:"ghost", #echo of medivh
+    26673:"ghost", #image of archmage modera
+    17468:"undead", #prophet velen
+    29481:"giant", #lok'lira the crone (inside)
+    30395:"ghost", #chieftain swiftspear
+    30074:"undead", #the leaper
+    29579:"comms", #brann bronzbeard (via communicator)
     28666:"undead", #gorebag
     28589:"demon", #gristlegut
     26527:"demon", #chromie (purging of stratholme)
@@ -302,7 +360,6 @@ NPC_EFFECTS = {
     26593:"demon", #serinar
     26983:"demon", #aurastrasza
     27763:"demon", #vargastrasz
-    28760:"undead", #hargus the gimp
     26653:"undead", #kilix the unraveler
     188419:"ghost", #elder mana'loa
     26500:"ghost", #image of drakuru
@@ -310,7 +367,15 @@ NPC_EFFECTS = {
     26701:"ghost", #image of drakuru
     26787:"ghost", #image of drakuru
     26924:"ghost", #gan'jo
+    31848:"demon", #zidormi
     37779:"undead", #dark ranger loralen
     37780:"undead", #dark ranger vorel
+    30304:"undead", #imhadria
+    32404:"ghost", #matthias lehner
+    31237:"ghost", #matthias lehner
+    32408:"ghost", #matthias lehner
+    32423:"ghost", #matthias lehner
+    32497:"ghost", #matthias lehner
+    40429:"demon", #sanctum guardian xerestrasza
 
 }
